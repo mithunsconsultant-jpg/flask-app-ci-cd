997cce3cff02943639cc0218973540fdf44f1109
misriniv <misriniv@cisco.com>
2025-08-10 19:34:08 +0530

    Edit message

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def hello_world():
-    return "Hello from flask and docker CI/CD with github actions"
+    return "Hello from flask and docker CI/CD with github actions. Mithun is testing"
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=3000)
